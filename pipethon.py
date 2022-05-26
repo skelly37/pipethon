91653f02a15f08e4296461cede01c3377ab9f21a
skelly37 <skelly37@protonmail.com>
2022-05-26 22:24:19 +0200

    1/2 work on windows done

--- pipethon.py
+++ pipethon.py
@@ -14,7 +14,7 @@ class Pipe:
 	NO_RESPONSE_MESSAGE = "No response from FIFO"
 	NOT_FOUND_MESSAGE = "FIFO doesn't exist"
 
-	def __init__(self, app_name: str, app_version: str, args):
+	def __init__(self, app_name: str, app_version: str, args=[]):
 		self.__app_name: str = app_name
 		self.__app_version: str = app_version
 		self.__platform: str = PLATFORM
@@ -23,17 +23,19 @@ class Pipe:
 
 		self.is_pipe_owner: bool = False
 
-		if self.__pipe_exists():
+		if self.__platform == "windows":
 			for arg in args:
 				if not self.send_to_pipe(arg):
-					os.unlink(self.path)
-					self.__create_pipe()
+					self.is_pipe_owner = True
+			#TODO generate read handler if Pipe is the actual pipe owner
 		else:
-			self.__create_pipe()
-
-		if self.__platform == "windows" and self.is_pipe_owner:
-			pass
-			#TODO windows pipe handle needed
+			if self.__pipe_exists():
+				for arg in args:
+					if not self.send_to_pipe(arg):
+						os.unlink(self.path)
+						self.__create_unix_pipe()
+			else:
+				self.__create_unix_pipe()
 
 
 	def __pipe_exists(self) -> bool:
@@ -49,50 +51,61 @@ class Pipe:
 		
 		return f"{prefix}{self.__app_name}_v{self.__app_version}_{username}_pipe_file"
 
-	def __create_pipe(self) -> None:
-		if self.__platform == "windows":
-			self.__create_win_pipe()
-		else:
-			self.__create_unix_pipe()
-
-		self.is_pipe_owner = True
-
-	def __create_win_pipe(self) -> None:
-		self.__windows_writer_handler = win32pipe.CreateNamedPipe(self.path,
-									  win32pipe.PIPE_ACCESS_DUPLEX,
-									  win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_WAIT,
-									  1, 65536, 65536, 300, None)
 
 	def __create_unix_pipe(self) -> None:
 		os.mkfifo(self.path)
+		self.is_pipe_owner = True
 
 
-	def send_to_pipe(self, message: str, timeout_secs: float = 1.5) -> bool:
-		if self.__platform == "windows":
-			return self.__send_to_win_pipe(message, timeout_secs)
-		else:
-			return self.__send_to_unix_pipe(message, timeout_secs)
+	def __win_sender(self, message: str) -> bool:
+		pipe = win32pipe.CreateNamedPipe(
+			self.path,
+			win32pipe.PIPE_ACCESS_DUPLEX,
+			win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+			1, 65536, 65536,
+			0,
+			None)
+		try:
+			win32pipe.ConnectNamedPipe(pipe, None)
+			win32file.WriteFile(pipe, str.encode(message))
+		finally:
+			win32file.CloseHandle(pipe)
 
-	def __send_to_win_pipe(self, message: str, timeout_secs: float) -> bool:
-		pass
-		#TODO
+		return True
 
 	def __unix_sender(self, message: str) -> bool:
 		with open(self.path, 'a') as fifo:
 			fifo.write(message)
 		return True
 
-	def __send_to_unix_pipe(self, message: str, timeout_secs: float) -> bool:
+	def send_to_pipe(self, message: str, timeout_secs: float = 1.5) -> bool:
 		__pool = concurrent.futures.ThreadPoolExecutor()
-		sender = __pool.submit(self.__unix_sender, message)
+		sender = None
+		if self.__platform == "windows":
+			sender = __pool.submit(self.__win_sender, message)
+		else:
+			sender = __pool.submit(self.__unix_sender, message)
 
 		try:
 			if sender.result(timeout=timeout_secs):
 				return True
 		except concurrent.futures._base.TimeoutError:
 			#hacky way to kill the sender
-			with open(self.path, 'r') as fifo:
-				fifo.read()
+
+			if self.__platform == "windows":
+				read_handle = win32file.CreateFile(
+					self.path,
+					win32file.GENERIC_READ | win32file.GENERIC_WRITE,
+					0,
+					None,
+					win32file.OPEN_EXISTING,
+					0,
+					None
+				)
+				win32file.ReadFile(read_handle, 64*1024)
+			else:
+				with open(self.path, 'r') as fifo:
+					fifo.read()
 
 		return False
 
@@ -135,8 +148,13 @@ class Pipe:
 
 		return Pipe.NO_RESPONSE_MESSAGE
 
-#TODO create win pipe (and import its module)
+
+#TODO create windows reader and handler (if the class actually owns the pipe)
+#TODO go through the code and find anything to improve
+
+
+
 p = Pipe(APP_NAME, VERSION, argv[1:])
-print(p.read_from_pipe())
+#print(p.read_from_pipe())
 print(p.is_pipe_owner)
 print(p.send_to_pipe("anything"))
